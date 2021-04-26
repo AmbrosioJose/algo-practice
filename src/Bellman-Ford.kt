770059a1893d5d69dc73dd38d26ddb8d52a3fe47
Jose Ambrosio <jjambrosio1@gmail.com>
2021-04-26 00:55:54 -0700

    Finished bellman for algorith

--- src/Bellman-Ford.kt
+++ src/Bellman-Ford.kt
@@ -37,6 +37,50 @@ Constraints:
 
 * */
 
-fun main(args: Array<Int>){
+class Edge(val src: Int, val dest: Int, val weight: Int)
+class Graph(val verticesCount: Int, val edgesCount: Int, val edges: List<Edge>)
 
+
+
+fun isNegativeWeightCycle(graph: Graph, src: Int): Boolean {
+    val dist: MutableList<Int> = MutableList(graph.edgesCount){ 0 }
+
+    // initialize all distances to vertexes to max value except for src
+    for( i in 0 until graph.verticesCount){
+        if(i != src)
+            dist[i] = Int.MAX_VALUE
+    }
+
+    // relax all edges v - 1 times
+    for( i in 0 until graph.edgesCount){
+        for(j: Int in 0 until dist.size){
+            val u = graph.edges[j].src
+            val v = graph.edges[j].dest
+            val w = graph.edges[j].weight
+
+            if(dist[u] != Int.MAX_VALUE && (dist[u] + w) < dist[v]) {
+                dist[v] = dist[u] + w
+            }
+        }
+    }
+
+    // check for negative-weight cycles. The above step guarantees shortest distances if graph doesn't contain negative weight cycle. If we get a shorter path then there is a negative-weight cycle
+    for(i in 0 until graph.edgesCount) {
+        val u: Int = graph.edges[i].src
+        val v: Int = graph.edges[i].dest
+        val weight: Int = graph.edges[i].weight
+
+        if(dist[u] != Integer.MAX_VALUE && dist[u] + weight < dist[v])
+            return true
+    }
+
+
+    return false
 }
+
+fun main(args: Array<String>){
+    val edges: List<Edge> = listOf(Edge(0,1,1), Edge(1, 2, -1), Edge(2, 3, -1), Edge(3, 0, -1))
+    val graph = Graph(4, 4, edges)
+    print("is negative weight cycle: ${isNegativeWeightCycle(graph, 0)}")
+}
+
