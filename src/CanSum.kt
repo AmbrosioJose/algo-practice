6e2caecefc83655f5339e7f1791be1b4a6daeabf
Jose Ambrosio <jjambrosio1@gmail.com>
2021-05-04 01:55:11 -0700

    finished can sum problem

---
+++ src/CanSum.kt
@@ -0,0 +1,34 @@
+import java.security.KeyStore
+
+/*
+*  Given a number and a list of numbers determine whether the target number can be obtained
+*  by summing numbers in the list of numbers
+*
+* */
+
+fun canSum(targetSum: Int, numbers:  List<Int>, sumMap: MutableMap<Int, Boolean> = mutableMapOf() ): Boolean{
+    if(sumMap.contains(targetSum) && sumMap[targetSum] != null) return sumMap.getValue(targetSum)
+    if(targetSum == 0) return true
+    if(targetSum < 0) return false
+
+    for(number in numbers){
+
+        if(number <= targetSum ){
+            val remainder = targetSum - number
+            if(canSum(remainder, numbers, sumMap)){
+                sumMap[targetSum] = true
+                return sumMap.getValue(targetSum)
+            }
+        }
+    }
+
+    sumMap[targetSum] = false
+    return sumMap.getValue(targetSum)
+}
+
+fun main(args: Array<String>){
+    println(canSum(7, listOf(2,3)))
+    println(canSum(7, listOf(5, 3, 4, 7)))
+    println(canSum(8, listOf(2, 3, 5)))
+    println(canSum(300, listOf(7, 14)))
+}
