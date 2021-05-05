080e8624ea0f1145e0e60f8ea1ee77689f390893
Jose Ambrosio <jjambrosio1@gmail.com>
2021-05-05 00:42:45 -0700

    Finished howSum and bestSum algorithms with memoizations

---
+++ src/bestSum.kt
@@ -0,0 +1,31 @@
+// Given the target number and a list of numbers find the smallest list of numbers that sum up
+// to the target number. If two are of equal length either is fine
+
+fun bestSum(targetSum: Int, numbers: List<Int>, sumMap: MutableMap<Int, MutableList<Int>?> = mutableMapOf()) : MutableList<Int>?{
+    if(sumMap.containsKey(targetSum)) return sumMap.getValue(targetSum)
+    if(targetSum == 0) return mutableListOf()
+    if(targetSum < 0) return null
+
+    var bestSum: MutableList<Int>? = null
+    for(number in numbers){
+        val remainder = targetSum - number
+        val listSum = bestSum(remainder, numbers, sumMap)
+        if(listSum != null){
+            // deep copy the list
+            val currentList = listSum.toMutableList()
+            currentList.add(number)
+            if(bestSum == null || bestSum.size > currentList.size){
+                bestSum = currentList
+            }
+        }
+    }
+    sumMap[targetSum] = bestSum
+    return bestSum
+}
+
+fun main(args: Array<String>){
+    println(bestSum(7, listOf(5, 3, 4, 7)))
+    println(bestSum(8, listOf(2, 3, 5)))
+    println(bestSum(8, listOf(1, 4, 5)))
+    println(bestSum(100, listOf(1, 2, 5, 25)))
+}
---
+++ src/howSum.kt
@@ -0,0 +1,28 @@
+// Given a target number and a list of numbers return a list of numbers that sum up to the target number
+
+fun howSum(targetSum: Int, numbers: List<Int>, sumMap: MutableMap<Int, MutableList<Int>?> = mutableMapOf()) : MutableList<Int>?{
+    if(sumMap.containsKey(targetSum)) return sumMap.getValue(targetSum)
+    if(targetSum == 0) return ArrayList()
+    if(targetSum < 0) return null
+
+    for(number in numbers){
+        val remainder = targetSum - number
+        val sumList = howSum(remainder, numbers, sumMap)
+        if( sumList != null){
+            sumList.add(number)
+            sumMap[targetSum] = sumList
+            return sumMap[targetSum]
+        }
+    }
+
+    sumMap[targetSum] = null
+    return sumMap[targetSum]
+}
+
+fun main(args: Array<String>){
+    println(howSum(7, listOf(2, 3)))
+    println(howSum(7, listOf(5, 3, 4, 7)))
+    println(howSum(7, listOf(2, 5)))
+    println(howSum(8, listOf(2, 3, 5)))
+    println(howSum(300, listOf(7, 14)))
+}
