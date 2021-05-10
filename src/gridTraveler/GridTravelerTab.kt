01ac0f9f62416227345ffea1903adc1c669370f6
Jose Ambrosio <jjambrosio1@gmail.com>
2021-05-10 02:02:04 -0700

    Changed from 2d list of Int to 2d list of Long because at 18x18 grid there is an overflow. Also did some clean up and optimized table instantiation

--- src/gridTraveler/GridTravelerTab.kt
+++ src/gridTraveler/GridTravelerTab.kt
@@ -1,10 +1,9 @@
 package gridTraveler
 
-fun gridTraveler(x: Int, y: Int): Int{
-    val table: MutableList<MutableList<Int>> = mutableListOf()
+fun gridTraveler(x: Int, y: Int): Long{
+    val table: MutableList<MutableList<Long>> = MutableList(y + 1){MutableList( x + 1) { 0L} }
     for(i : Int in  0 ..y){
-        table.add(MutableList(x + 1) {0})
-
+        table.add(MutableList(x + 1) {0L})
     }
 
     table[1][1] = 1
@@ -16,30 +15,13 @@ fun gridTraveler(x: Int, y: Int): Int{
         }
     }
 
-//    for(i in 0..y){
-//        for(j in 0..x){
-//            val current = table[i][j]
-//
-//            if((j + 1) < x) table[i][j + 1] += current
-//            if((i + 1) < y) table[i + 1][j] += current
-//            if((j + 1) < x && (i + 1) < y)
-//                println("current $current, i: $i, j: $j newJ:${table[i][j + 1]}, newi: ${table[i + 1][j]}")
-//            else if((j + 1) < x)
-//                println("current $current, i: $i, j: $j newJ:${table[i][j + 1]}")
-//            else if((i + 1) < x)
-//                println("current $current, i: $i, j: $j newi: ${table[i + 1][j]}")
-//
-//        }
-//    }
-
-    print(table)
     return table[y][x]
 }
 
 fun main(args: Array<String>){
-//    println(gridTraveler(1 , 1))
+    println(gridTraveler(1 , 1))
     println(gridTraveler(2 , 3))
-//    println(gridTraveler(3 , 2))
-//    println(gridTraveler(3 , 3))
-//    println(gridTraveler(18 , 18))
+    println(gridTraveler(3 , 2))
+    println(gridTraveler(3 , 3))
+    println(gridTraveler(18 , 18))
 }
