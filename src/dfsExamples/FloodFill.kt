e448dda36faec2418980407708536ea11e54cc05
AmbrosioJose <jjambrosio1@gmail.com>
2022-06-29 00:48:10 -0700

    adds more bfs examples

--- src/dfsExamples/FloodFill.kt
+++ src/dfsExamples/FloodFill.kt
@@ -1,4 +1,78 @@
 package dfsExamples
 
+/*
+*
+* An image is represented by an m x n integer grid image where image[i][j] represents the pixel value of the image.
+
+You are also given three integers sr, sc, and color. You should perform a flood fill on the image starting from the pixel image[sr][sc].
+
+To perform a flood fill, consider the starting pixel, plus any pixels connected 4-directionally to the starting pixel of the same color as the starting pixel, plus any pixels connected 4-directionally to those pixels (also with the same color), and so on. Replace the color of all of the aforementioned pixels with color.
+
+Return the modified image after performing the flood fill.
+
+
+
+Example 1:
+
+
+Input: image = [[1,1,1],[1,1,0],[1,0,1]], sr = 1, sc = 1, color = 2
+Output: [[2,2,2],[2,2,0],[2,0,1]]
+Explanation: From the center of the image with position (sr, sc) = (1, 1) (i.e., the red pixel), all pixels connected by a path of the same color as the starting pixel (i.e., the blue pixels) are colored with the new color.
+Note the bottom corner is not colored 2, because it is not 4-directionally connected to the starting pixel.
+Example 2:
+
+Input: image = [[0,0,0],[0,0,0]], sr = 0, sc = 0, color = 0
+Output: [[0,0,0],[0,0,0]]
+Explanation: The starting pixel is already colored 0, so no changes are made to the image.
+*
+* */
+
+class Position(r: Int, c: Int){}
 class FloodFill {
+    // directional vectors
+    val rowDirectional = intArrayOf(-1 , 1, 0, 0)
+    val columnDirectional = intArrayOf(0, 0, 1, -1)
+
+    fun floodFillImage(image: Array<IntArray>, sr: Int, sc: Int, color: Int) : Array<IntArray>{
+
+        val visited = Array(image.size) {Array<Boolean>(image[0].size){false}}
+
+        visited[sr][sc] = true
+        fill4Directionally(image, visited, sr, sc, color)
+
+        return image
+    }
+
+    private fun fill4Directionally(image: Array<IntArray>, visited: Array<Array<Boolean>>, sr: Int, sc: Int, color: Int){
+        println("Checking row $sr column: $sc")
+
+        for(i in rowDirectional.indices){
+            val connectedRowIndex = sr + rowDirectional[i]
+            val connectedColumnIndex = sc + columnDirectional[i]
+
+            // Check for out of bounds
+            if(connectedRowIndex < 0 || connectedColumnIndex < 0) continue
+            if(connectedRowIndex >= image.size || connectedColumnIndex >= image.first().size) continue
+
+            // check visited
+            if(visited[connectedRowIndex][connectedColumnIndex]) continue
+
+            visited[connectedRowIndex][connectedColumnIndex] = true
+
+            if(image[connectedRowIndex][connectedColumnIndex] == image[sr][sc])
+                fill4Directionally(image, visited, connectedRowIndex, connectedColumnIndex, color)
+        }
+
+
+        image[sr][sc] = color
+    }
+
+}
+
+fun main(){
+    val image = arrayOf(intArrayOf(1,1,1), intArrayOf(1,1,0), intArrayOf(1,0,1))
+    println(image.contentDeepToString())
+    FloodFill().floodFillImage(image, 1, 1, 2)
+    print("updated image: ")
+    println(image.contentDeepToString())
 }
--- src/dfsExamples/MaxAreaOfIsland.kt
+++ src/dfsExamples/MaxAreaOfIsland.kt
@@ -1,4 +1,76 @@
 package dfsExamples
+/*You are given an m x n binary matrix grid. An island is a group of 1's (representing land) connected 4-directionally (horizontal or vertical.) You may assume all four edges of the grid are surrounded by water.
 
+The area of an island is the number of cells with a value 1 in the island.
+
+Return the maximum area of an island in grid. If there is no island, return 0.
+
+
+
+Example 1:
+
+
+Input: grid = [[0,0,1,0,0,0,0,1,0,0,0,0,0],[0,0,0,0,0,0,0,1,1,1,0,0,0],[0,1,1,0,1,0,0,0,0,0,0,0,0],[0,1,0,0,1,1,0,0,1,0,1,0,0],[0,1,0,0,1,1,0,0,1,1,1,0,0],[0,0,0,0,0,0,0,0,0,0,1,0,0],[0,0,0,0,0,0,0,1,1,1,0,0,0],[0,0,0,0,0,0,0,1,1,0,0,0,0]]
+Output: 6
+Explanation: The answer is not 11, because the island must be connected 4-directionally.
+Example 2:
+
+Input: grid = [[0,0,0,0,0,0,0,0]]
+Output: 0
+
+
+Constraints:
+
+m == grid.length
+n == grid[i].length
+1 <= m, n <= 50
+grid[i][j] is either 0 or 1.
+*/
 class MaxAreaOfIsland {
+    val rowDirectional = intArrayOf(-1, 1, 0, 0)
+    val columnDirectional = intArrayOf(0, 0, 1, -1)
+
+    fun maxAreaOfIsland(grid: Array<IntArray>): Int {
+        val visited = Array(grid.size){Array(grid.first().size){false}}
+        var maximumArea = 0
+
+        grid.forEachIndexed{ r, column ->
+            column.forEachIndexed{ c, gridValue ->
+                if(!visited[r][c] && gridValue == 1){
+                    visited[r][c] = true
+                    val areaOfCurrentIsland = islandAreaAt(grid, visited, r, c)
+                    if(areaOfCurrentIsland > maximumArea)
+                        maximumArea = areaOfCurrentIsland
+                }
+            }
+        }
+
+        return maximumArea
+    }
+
+    private fun islandAreaAt(grid: Array<IntArray>, visited: Array<Array<Boolean>>, r: Int, c: Int) : Int{
+        var area = 1
+
+        // get area of connected land
+        for(i in rowDirectional.indices){
+            val connectedR = r + rowDirectional[i]
+            val connectedC = c + columnDirectional[i]
+
+            // Skip out of bounds
+            if(connectedR < 0 || connectedC < 0) continue
+            if(connectedR >= grid.size || connectedC >= grid.first().size) continue
+
+            // Skip already visited
+            if(visited[connectedR][connectedC]) continue
+            visited[connectedR][connectedC] = true
+
+            // Skip water areas
+            if(grid[connectedR][connectedC] == 0) continue
+
+            area += islandAreaAt(grid, visited, connectedR, connectedC)
+        }
+
+
+        return area
+    }
 }
